6a5f8b65f4dfb8989597083fd01d297aa2beb872
awalramdhan <152985419+Aloy24@users.noreply.github.com>
2023-12-13 16:23:58 +0800

    Add files via upload
    upload code program

---
+++ pratikum2.py
@@ -0,0 +1,399 @@
+print('pratikum-b2')
+
+print('nama lengkap:muhammad nurakmal')
+print('nim         :231031055')
+print('prodi       :sistem informasi')
+
+print()
+a=15
+print('nilai a adalah=',a)
+a+=2
+print('nilai a menjadi=',a)
+print('nilai a+=2 menjadi',a)
+
+
+print()
+a=15
+print('nilai a adalah=',a)
+a-=2
+print('nilai a menjadi=',a)
+print('nilai a-=2 menjadi=',a)
+
+print()
+a=15
+print('nilai a adalah=',a)
+a*=2   #ini operasi a=a*2
+print('nilai a menjadi=',a)
+print('nilai a*=2 menjadi=',a)
+
+print()
+a=5
+print('nilai a adalah=',a)
+a**=2
+print('nilai a**=2menjadi=',a)
+
+
+print()
+a=35
+print('nilai a adalah=',a)
+a%=31
+print('nilai a%%=2menjadi=',a)
+
+print()
+a=35
+print('nilai a adalah=',a)
+a//=31
+print('nilai a//=2menjadi=',a)
+
+
+
+
+bi=True
+print('nilai bi adalah=',bi)
+bi=bi  | False
+print('nilai bi menjadi=',bi)
+
+
+bi=False
+print('nilai bi adalah=',bi)
+bi |= True
+print('nilai bi|=True menjadi=',bi)
+
+print()
+bi=False
+print('nilai bi adalah=',bi)
+bi &= True
+print('nilai bi&=True menjadi=',bi)
+
+
+print()
+bi=False
+print('nilai bi adalah=',bi)
+bi^= True
+print('nilai bi^=True menjadi=',bi)
+
+
+print()
+a=7
+print('nilai adalah=',a)
+
+
+hasil=a==5
+print('apakah a=5?',hasil)
+hasil=a>9
+print('apakah a>9?',hasil)
+hasil=a<9
+print('apakah a<9?',hasil)
+hasil=a !=5
+print('apakah a !=5?',hasil)
+
+
+
+hasil=a==7
+print('apakah a=7?',hasil)
+hasil=a>7
+print('apakah a>7?',hasil)
+hasil=a<7
+print('apakah a<7?',hasil)
+hasil=a !=7
+print('apakah a!=7?',hasil)
+hasil=a>=7
+print('apakah a>=7?',hasil)
+hasil=a<=7
+print('apakah a<=7?',hasil)
+
+
+
+
+a=True
+b=False
+
+hasil=a and a
+print(a,'and',a,'adalah=',hasil)
+hasil=a and b
+print(a,'and',b,'adalah=',hasil)
+hasil=b and a
+print(b,'and',a,'adalah=',hasil)
+hasil=b and b
+print(b,'and',b,'adalah=',hasil)
+
+
+
+
+
+
+hasil=a or a
+print(a,'or',a,'adalah=',hasil)
+hasil=a or b
+print(a,'or',b,'adalah=',hasil)
+hasil=b or a
+print(b,'or',a,'adalah=',hasil)
+hasil=b or b
+print(b,'or',b,'adalah=',hasil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
