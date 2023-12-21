17db7d8d69f429e389b68a21e682fffbb63de5a6
awalramdhan <152985419+Aloy24@users.noreply.github.com>
2023-12-21 20:33:20 +0800

    Add files via upload

---
+++ praktikum-b2.py
@@ -0,0 +1,139 @@
+print('praktikum-b2')
+
+print('NAMA LENGKAP:AWAL RAMADHAN N')
+print('NIM         :231031040')
+print('prodi       :sistem informasi')
+
+print()
+a=15
+print('nilai a adalah=',a)
+a+=2
+print('nilai a menjadi=',a)
+
+print()
+a=15
+print('nilai a adalah=',a)
+a+=2
+print('nilai a+=2 menjadi=',a)
+
+print()
+a=15
+print('nilai a adalah=',a)
+a-=2
+print('nilai a-=2 menjadi=',a)
+
+print()
+a=15
+print('nilai a adalah=',a)
+a*=2 #ini operasi a=a*2
+print('nilai a*=2 menjadi=',a)
+
+print()
+a=15
+print('nilai a adalah=',a)
+a/=2 #ini operasi a=a/2
+print('nilai a/=2 menjadi=',a)
+
+print()
+a=5
+print('nilai a adalah=',a)
+a**=2 #ini operasi a=a**2
+print('nilai a**=2 menjadi=',a)
+
+print()
+a=35
+print('nilai a adalah=',a)
+a%=31 #ini operasi a=a%7
+print('nilai a%=7 menjadi=',a)
+
+print()
+a=35
+print('nilai a adalah=',a)
+a//=31 #ini operasi a=a//7
+print('nilai a//=7 menjadi=',a)
+
+######################
+print()
+bi=False
+print('Nilai bi adalah',bi)
+bi=bi|True
+print('Nilai bi menjadi=',bi)
+
+print()
+bi=False
+print('Nilai bi adalah=',bi)
+bi&=True
+print('Nilai bi&=true menjadi=',bi)
+
+print()
+bi=False
+print('Nilai bi adalah=',bi)
+bi^=True
+print('Nilai bi^=true menjadi=',bi)
+
+print()
+bi=True
+print('Nilai bi adalah=',bi)
+bi^=True
+print('Nilai bi^=true menjadi=',bi)
+#  #####################
+print()
+a=7
+print('nilai adalah=',a)
+hasil=a==7
+print('apakah a>7?',hasil)
+hasil=a<7
+print('apakah a>7?',hasil)
+hasil=a<7
+print('apakah a<7?',hasil)
+hasil=a!=7
+print('apakah!=7?',hasil)
+hasil=a>=7
+print('apakah a>=7?',hasil)
+hasil=a<=7
+print('apakah a<=7?',hasil)
+
+######################
+a=True
+b=False
+
+hasil=a and a
+print(a,'and',a,'adalah=',hasil)
+hasil=a and b
+print(a,'and',b,'adalah=',hasil)
+hasil=b and a
+print(b,'and',a,'adalah=',hasil)
+hasil=b and b
+print(b,'and',b,'adalah=',hasil)
+
+print()
+hasil=a or a
+print(a,'or',a,'adalah=',hasil)
+hasil=a or b
+print(a,'or',b,'adalah=',hasil)
+hasil=b or a
+print(b,'or',a,'adalah=',hasil)
+hasil=b or b
+print(b,'or',b,'adalah=',hasil)
+
+print()
+hasil=a ^ a
+print(a,'xor',a,'adalah=',hasil)
+hasil=a ^ b
+print(a,'xor',b,'adalah=',hasil)
+hasil=b ^ a
+print(b,'xor',a,'adalah=',hasil)
+hasil=b ^ b
+print(b,'xor',b,'adalah=',hasil)
+
+print()
+hasil=not a
+print('not',a,'adalah=',hasil)
+hasil=not b
+print('not',b,'adalah=',hasil)      
+hasil=not a
+print('not',a,'adalah=',hasil)
+hasil=not b
+print('not',b,'adalah=',hasil)
+
+
