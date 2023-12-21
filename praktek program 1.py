006e9fbd9e81561c865163bb14c3c3702b8e53a4
awalramdhan <152985419+Aloy24@users.noreply.github.com>
2023-12-21 20:31:00 +0800

    Add files via upload

---
+++ praktek program 1.py
@@ -0,0 +1,78 @@
+print('praktikum-1')
+print('nama lengkap: AWAL RAMADHAN')
+print('nim         : 231031040')
+print('prodi       : sistem informasi')
+print()
+
+angkatan = 2023
+print('angkatan adalah '+str(angkatan))
+print()
+a=20
+print('data adalah',a)
+print('tipe data adalah',type(a))
+
+angkatan = 2023
+print('angkatan adalah '+str(angkatan))
+print()
+b=20,0
+print('data adalah',b)
+print('tipe data adalah',type(b))
+
+print()
+kampus='institut teknologi BJ habibie'
+print('data adalah',kampus)
+print('tipe data adalah',type(kampus))
+
+print()
+log= True
+print('data adalah',log)
+print('tipe data adalah',type(log))
+
+print()
+c=complex(2,3)
+print('data adalah',c)
+print('tipe data adalah',type(c))
+
+print()
+p=10
+q=5
+hasil=p+q
+print('hasil',p,'+',q,'=',hasil)
+
+
+print()
+p=10
+q=5
+hasil=p-q
+print('hasil',p,'-',q,'=',hasil)
+
+
+print()
+p=10
+q=5
+hasil=p*q
+print('hasil',p,'*',q,'=',hasil)
+
+print()
+p=10
+q=5
+hasil=p/q
+print('hasil',p,'/',q,'=',hasil)
+
+print()
+p=10
+q=5
+hasil=p%q
+print('hasil',p,'%',q,'=',hasil)
+
+print()
+p=10
+q=5
+hasil=p//q
+print('hasil',p,'//',q,'=',hasil)
+
+print()
+p=4
+q=3
+hasil=p**q
+print('hasil',p,'**',q,'=',hasil)
